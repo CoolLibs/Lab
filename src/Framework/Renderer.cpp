c8a654a84a4bf4670cc6f47e09604523fa90b390
Jules Fouchy <jules.fouchy@ntymail.com>
2020-10-10 22:50:16 +0200

    Cleanup Renderer

--- src/Framework/Renderer.cpp
+++ src/Framework/Renderer.cpp
@@ -14,11 +14,6 @@ void Renderer::end() {
 	m_renderBuffer.unbind();
 }
 
-void Renderer::onRenderAreaResized() {
-	const glm::ivec2& size = Viewports::Size().size();
-	m_renderBuffer.setSize(size.x, size.y);
-}
-
 void Renderer::drawFullScreen() {
 	m_fullScreenVAO.bind();
 	GLCall(glDrawArrays(GL_TRIANGLES, 0, 3));
@@ -27,4 +22,9 @@ void Renderer::drawFullScreen() {
 void Renderer::drawFullScreenWithUVs() {
 	m_fullScreenVAOWithUVs.bind();
 	GLCall(glDrawArrays(GL_TRIANGLES, 0, 3));
+}
+
+void Renderer::onRenderAreaResized() {
+	const glm::ivec2& size = Viewports::Size().size();
+	m_renderBuffer.setSize(size.x, size.y);
 }
--- src/Framework/Renderer.h
+++ src/Framework/Renderer.h
@@ -1,24 +1,24 @@
 #pragma once
 
-#include "OpenGL/QuadVAO.h"
 #include "OpenGL/RenderBufferFB.h"
+#include "OpenGL/QuadVAO.h"
 
 class Renderer {
 public:
 	Renderer();
 	~Renderer() = default;
-	Renderer(const Renderer& other) = delete;			 // non-copyable
-	Renderer& operator=(const Renderer& other) = delete; // non-copyable
+	Renderer(const Renderer& other) = delete;			 // non-copyable because QuadVAO is not quite copyable at the moment
+	Renderer& operator=(const Renderer& other) = delete; // non-copyable because QuadVAO is not quite copyable at the moment
 
 	void begin();
 	void end();
-
-	void onRenderAreaResized();
 
 	void drawFullScreen();
 	void drawFullScreenWithUVs();
 
 	inline RenderBufferFB& renderBuffer() { return m_renderBuffer; }
+
+	void onRenderAreaResized();
 
 private:
 	RenderBufferFB m_renderBuffer;
