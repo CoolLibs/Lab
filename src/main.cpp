212e13cbd8088dfce3360f3c9f2ed802b111ac8c
Jules Fouchy <jules.fouchy@ntymail.com>
2020-10-10 22:41:06 +0200

    Cleanup main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,94 +5,94 @@
 #include <imgui_impl_opengl3.h>
 
 int main(int argc, char *argv[]) {
-	// Choose OpenGL version
+	// OpenGL version
 	constexpr int glMajorVersion = 4;
 	constexpr int glMinorVersion = 3;
 	constexpr char* glslVersion = "#version 430";
-		// ---------- Initialize spdlog ---------------
+	// ---------- Initialize spdlog ---------------
 
-		Log::Initialize();
+	Log::Initialize();
 
-		// ------- Initialize SDL and OpenGL ------------
+	// ------- Initialize SDL and OpenGL ------------
 
-		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
-			Log::Error("Unable to initialize SDL : {}", SDL_GetError());
-		}
-		SDL_GL_LoadLibrary(NULL);
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+		Log::Error("Unable to initialize SDL : {}", SDL_GetError());
+	}
+	SDL_GL_LoadLibrary(NULL);
 #if __APPLE__
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
 #else
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
 #endif
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, glMajorVersion);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, glMinorVersion);
-		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-		SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-		SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, glMajorVersion);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, glMinorVersion);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
 
-		// Window
-		SDL_Window* window = SDL_CreateWindow(
-			"Template Project",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-			1280, 720,
-			SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI 
+	// Window
+	SDL_Window* window = SDL_CreateWindow(
+		"Template Project",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		1280, 720,
+		SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI 
 #ifdef NDEBUG
-			| SDL_WINDOW_MAXIMIZED
+		| SDL_WINDOW_MAXIMIZED
 #endif
-		);
-		if (window == nullptr) {
-			Log::Error("Failed to create window : {}", SDL_GetError());
+	);
+	if (window == nullptr) {
+		Log::Error("Failed to create window : {}", SDL_GetError());
+	}
+	GLWindow glWindow(window);
+	glWindow.makeCurrent();
+	//
+	SDL_GL_SetSwapInterval(1);
+
+	if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
+		Log::Error("Glad failed to initialize");
+	}
+
+	// ------- Initialize ImGUI ------------
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui::StyleColorsClassic();
+
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+	// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
+
+	ImGui_ImplSDL2_InitForOpenGL(glWindow.window, glWindow.glContext);
+	ImGui_ImplOpenGL3_Init(glslVersion);
+
+	// ------ Initialize our own classes
+
+	// ------ Actual App
+	{
+		App app;
+		AppFramework appFramework(glWindow, app);
+		while (!appFramework.shouldClose()) {
+			appFramework.update();
 		}
-		GLWindow glWindow(window);
-		glWindow.makeCurrent();
-		//
-		SDL_GL_SetSwapInterval(1);
+	} // Needs to be in a scope so that app is destructed here. Otherwise console won't close
 
-		if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
-			Log::Error("Glad failed to initialize");
-		}
-
-		// ------- Initialize ImGUI ------------
-
-		IMGUI_CHECKVERSION();
-		ImGui::CreateContext();
-		ImGui::StyleColorsClassic();
-
-		ImGuiIO& io = ImGui::GetIO();
-		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
-
-		// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
-		ImGuiStyle& style = ImGui::GetStyle();
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-		{
-			style.WindowRounding = 0.0f;
-			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-		}
-
-		ImGui_ImplSDL2_InitForOpenGL(glWindow.window, glWindow.glContext);
-		ImGui_ImplOpenGL3_Init(glslVersion);
-
-		// ------ Initialize our own classes
-
-		// ------ Actual App
-		{
-			App app;
-			AppFramework appFramework(glWindow, app);
-			while (!appFramework.shouldClose()) {
-				appFramework.update();
-			}
-		} // Needs to be in a scope so that app is destructed here. Otherwise console won't close
-
-		// ------ Shutdown
-		ImGui_ImplOpenGL3_Shutdown();
-		ImGui_ImplSDL2_Shutdown();
-		ImGui::DestroyContext();
-		glWindow.destroy();
-		SDL_Quit();
+	// ------ Shutdown
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+	glWindow.destroy();
+	SDL_Quit();
 
 	return 0;
 }
