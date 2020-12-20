23faafe52a0896e61e3bcac3db4fa88e261862e2
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-20 15:44:37 +0100

    Input : add comments to explain the coordinate system

--- src/Framework/Input.h
+++ src/Framework/Input.h
@@ -3,8 +3,12 @@
 class Input {
 public:
 	static bool KeyIsDown(SDL_Scancode key);
-	// Relative to the viewing area, not the whole window !
+	/// Relative to the viewing area, not the whole window !
+	/// (0, 0) is in the top-left corner
 	static glm::ivec2 MouseInPixels();
-	// Relative to the viewing area, not the whole window !
+	/// Relative to the viewing area, not the whole window !
+	/// (0, 0) is in the middle
+	/// x is in the range [-aspectRatio, +aspectRatio] Axis pointing right
+	/// y is in the range [-1, 1] Axis poiting up
 	static glm::vec2  MouseInNormalizedRatioSpace();
 };
