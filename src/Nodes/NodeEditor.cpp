786eeff9f73dcad212fbbceb5ac4a7d88ecfcbf9
Jules Fouchy <jules.fouchy@ntymail.com>
2021-05-07 21:24:57 +0200

    Update imgui_node_editor

--- src/Nodes/NodeEditor.cpp
+++ src/Nodes/NodeEditor.cpp
@@ -16,17 +16,123 @@ void NodeEditor::ImGui_window()
     ed::Begin("My Editor");
     int uniqueId = 1;
 
-    // Start drawing nodes.
-    ed::BeginNode(uniqueId++);
-        ImGui::Text("Node A");
-        ed::BeginPin(uniqueId++, ed::PinKind::Input);
-            ImGui::Text("-> In");
-        ed::EndPin();
-        ImGui::SameLine();
-        ed::BeginPin(uniqueId++, ed::PinKind::Output);
-            ImGui::Text("Out ->");
-        ed::EndPin();
+    //
+    // 1) Commit known data to editor
+    //
+
+    // Submit Node A
+    ed::NodeId nodeA_Id = uniqueId++;
+    ed::PinId  nodeA_InputPinId = uniqueId++;
+    ed::PinId  nodeA_OutputPinId = uniqueId++;
+
+    ed::BeginNode(nodeA_Id);
+    ImGui::Text("Node A");
+    ed::BeginPin(nodeA_InputPinId, ed::PinKind::Input);
+    ImGui::Text("-> In");
+    ed::EndPin();
+    ImGui::SameLine();
+    ed::BeginPin(nodeA_OutputPinId, ed::PinKind::Output);
+    ImGui::Text("Out ->");
+    ed::EndPin();
     ed::EndNode();
+
+    // Submit Node B
+    ed::NodeId nodeB_Id = uniqueId++;
+    ed::PinId  nodeB_InputPinId1 = uniqueId++;
+    ed::PinId  nodeB_InputPinId2 = uniqueId++;
+    ed::PinId  nodeB_OutputPinId = uniqueId++;
+
+    ed::BeginNode(nodeB_Id);
+    ImGui::Text("Node B");
+    ImGui::BeginGroup();
+    ed::BeginPin(nodeB_InputPinId1, ed::PinKind::Input);
+    ImGui::Text("-> In1");
+    ed::EndPin();
+    ed::BeginPin(nodeB_InputPinId2, ed::PinKind::Input);
+    ImGui::Text("-> In2");
+    ed::EndPin();
+    ImGui::EndGroup();
+    ImGui::SameLine();
+    ImGui::BeginGroup();
+    ed::BeginPin(nodeB_OutputPinId, ed::PinKind::Output);
+    ImGui::Text("Out ->");
+    ed::EndPin();
+    ImGui::EndGroup();
+    ed::EndNode();
+
+    // Submit Links
+    for (auto& linkInfo : _links)
+        ed::Link(linkInfo.Id, linkInfo.InputId, linkInfo.OutputId);
+
+    //
+    // 2) Handle interactions
+    //
+
+    // Handle creation action, returns true if editor want to create new object (node or link)
+    if (ed::BeginCreate())
+    {
+        ed::PinId inputPinId, outputPinId;
+        if (ed::QueryNewLink(&inputPinId, &outputPinId))
+        {
+            // QueryNewLink returns true if editor want to create new link between pins.
+            //
+            // Link can be created only for two valid pins, it is up to you to
+            // validate if connection make sense. Editor is happy to make any.
+            //
+            // Link always goes from input to output. User may choose to drag
+            // link from output pin or input pin. This determine which pin ids
+            // are valid and which are not:
+            //   * input valid, output invalid - user started to drag new ling from input pin
+            //   * input invalid, output valid - user started to drag new ling from output pin
+            //   * input valid, output valid   - user dragged link over other pin, can be validated
+
+            if (inputPinId && outputPinId) // both are valid, let's accept link
+            {
+                // ed::AcceptNewItem() return true when user release mouse button.
+                if (ed::AcceptNewItem())
+                {
+                    // Since we accepted new link, lets add one to our list of links.
+                    _links.push_back({ ed::LinkId(_next_link_id++), inputPinId, outputPinId });
+
+                    // Draw new link.
+                    ed::Link(_links.back().Id, _links.back().InputId, _links.back().OutputId);
+                }
+
+                // You may choose to reject connection between these nodes
+                // by calling ed::RejectNewItem(). This will allow editor to give
+                // visual feedback by changing link thickness and color.
+            }
+        }
+    }
+    ed::EndCreate(); // Wraps up object creation action handling.
+
+
+    // Handle deletion action
+    if (ed::BeginDelete())
+    {
+        // There may be many links marked for deletion, let's loop over them.
+        ed::LinkId deletedLinkId;
+        while (ed::QueryDeletedLink(&deletedLinkId))
+        {
+            // If you agree that link can be deleted, accept deletion.
+            if (ed::AcceptDeletedItem())
+            {
+                // Then remove link from your data.
+                for (auto& link : _links)
+                {
+                    if (link.Id == deletedLinkId)
+                    {
+                        _links.erase(&link);
+                        break;
+                    }
+                }
+            }
+
+            // You may reject link deletion by calling:
+            // ed::RejectDeletedItem();
+        }
+    }
+    ed::EndDelete(); // Wrap up deletion action
 
     ed::End();
     ImGui::End();
--- src/Nodes/NodeEditor.h
+++ src/Nodes/NodeEditor.h
@@ -4,6 +4,12 @@
 
 namespace ed = ax::NodeEditor;
 
+struct LinkInfo {
+	ed::LinkId Id;
+	ed::PinId  InputId;
+	ed::PinId  OutputId;
+};
+
 class NodeEditor {
 public:
 	NodeEditor();
@@ -12,5 +18,7 @@ public:
 	void ImGui_window();
 
 private:
-	ed::EditorContext* _context;
+	ed::EditorContext* _context; 
+	ImVector<LinkInfo> _links;
+	int _next_link_id = 100; // Counter to help generate link ids. In real application this will probably based on pointer to user data structure.
 };
