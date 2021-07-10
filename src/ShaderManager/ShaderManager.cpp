c1a318acf6c8bbe403445796532c1291e84c6602
Jules Fouchy <jules.fouchy@ntymail.com>
2021-07-11 00:30:25 +0200

    [ShaderManager::setup_for_rendering] Change the uniform names

--- src/ShaderManager/ShaderManager.cpp
+++ src/ShaderManager/ShaderManager.cpp
@@ -6,11 +6,11 @@
 
 void ShaderManager::setup_for_rendering(const Camera& camera, float focal_length) {
 	_shader.bind();
-	_shader.set_uniform("uAspectRatio", RenderState::Size().aspectRatio());
-	_shader.set_uniform("uCamX", camera.right_axis());
-	_shader.set_uniform("uCamY", camera.up_axis());
-	_shader.set_uniform("uCamZ", camera.front_axis());
-	_shader.set_uniform("uCamPos", camera.position());
-	_shader.set_uniform("uFocalLength", focal_length);
-	_shader.set_uniform("uTime", Time::time());
+	_shader.set_uniform("_aspect_ratio", RenderState::Size().aspectRatio());
+	_shader.set_uniform("_camera_right_axis", camera.right_axis());
+	_shader.set_uniform("_camera_up_axis", camera.up_axis());
+	_shader.set_uniform("_camera_front_axis", camera.front_axis());
+	_shader.set_uniform("_camera_position", camera.position());
+	_shader.set_uniform("_focal_length", focal_length);
+	_shader.set_uniform("_time", Time::time());
 }
