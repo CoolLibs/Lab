0acf3c6e93253f4358ba3c7bc97a85f11fd693d7
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-06 14:59:55 +0200

    [Cool] Move a lot of things to Cool::DefaultApp

--- src/App.cpp
+++ src/App.cpp
@@ -1,28 +1,15 @@
 #include "App.h"
-#include <Cool/Input/Input.h>
-#include <Cool/Log/ToUser.h>
-#include <Cool/Parameters/ParametersHistory.h>
-#include <Cool/Time/ClockU.h>
 
 App::App(Cool::WindowManager& windows)
-    : _main_window{windows.main_window()}
-    , _view{_views.make_view("View")}
+    : DefaultApp::DefaultApp{windows, [&](Cool::RenderTarget& render_target, float time) { render(render_target, time); }}
 {
-    _camera.hook_events(_view.view.mouse_events());
 }
 
 void App::update()
 {
-    if (!_exporter.is_exporting()) {
-        _clock.update();
+    DefaultApp::update();
+    if (inputs_are_allowed()) {
         _shader_manager->update();
-        for (auto& view : _views) {
-            view.update_size(_preview_constraint);
-        }
-        polaroid().render(_clock.time());
-    }
-    else {
-        _exporter.update(polaroid());
     }
 }
 
@@ -43,130 +30,35 @@ void App::render(Cool::RenderTarget& render_target, float time)
 #endif
 }
 
-Cool::Polaroid App::polaroid()
-{
-    return {
-        .render_target = _view.render_target,
-        .render_fn     = [&](Cool::RenderTarget& render_target, float time) {
-            render(render_target, time);
-        }};
-}
-
-bool App::aspect_ratio_is_constrained() const
-{
-    return _exporter.is_exporting() ||
-           _preview_constraint.wants_to_constrain_aspect_ratio();
-}
-
-bool App::inputs_are_allowed() const
-{
-    return !_exporter.is_exporting();
-}
-
-bool App::wants_to_show_menu_bar() const
-{
-    return !_exporter.is_exporting();
-}
-
 void App::imgui_windows()
 {
-    // Views
-    for (auto& view : _views) {
-        view.imgui_window(aspect_ratio_is_constrained());
-    }
-    // Exporter
-    _exporter.imgui_windows(polaroid(), _clock.time());
-    //
-    if (!_exporter.is_exporting()) {
-        // Console
-        Log::ToUser::imgui_console_window();
-        // Time
-        ImGui::Begin("Time");
-        Cool::ClockU::imgui_timeline(_clock);
-        ImGui::End();
-        // Camera
-        ImGui::Begin("Camera");
-        _camera.imgui();
-        ImGui::End();
-        // ShaderManager
+    DefaultApp::imgui_windows();
+    if (inputs_are_allowed()) {
         _shader_manager.ImGui_windows();
-#if defined(DEBUG)
-        if (_show_imgui_debug) {
-            ImGui::Begin("Debug", &_show_imgui_debug);
-            ImGui::Text("%.1f FPS", ImGui::GetIO().Framerate);
-            _main_window.imgui_cap_framerate();
-            ImGui::Checkbox("Show Demo Window", &_show_imgui_demo);
-            ImGui::End();
-        }
-        if (_show_imgui_demo) { // Show the big demo window (Most of the sample code is
-                                // in ImGui::ShowDemoWindow()! You can browse its code
-                                // to learn more about Dear ImGui!).
-            ImGui::ShowDemoWindow(&_show_imgui_demo);
-        }
-#endif
     }
 }
 
 void App::imgui_menus()
 {
-    if (ImGui::BeginMenu("Preview")) {
-        _preview_constraint.imgui();
-        ImGui::EndMenu();
-    }
-    if (ImGui::BeginMenu("Windows")) {
-        Cool::Log::ToUser::imgui_toggle_console();
-        for (auto& view : _views) {
-            view.view.imgui_open_close_checkbox();
-        }
-#if defined(DEBUG)
-        ImGui::Separator();
-        ImGui::Checkbox("Debug", &_show_imgui_debug);
-#endif
-        ImGui::EndMenu();
-    }
-    if (ImGui::BeginMenu("Export")) {
-        _exporter.imgui_menu_items();
-        ImGui::EndMenu();
-    }
+    DefaultApp::imgui_menus();
 }
 
 void App::on_keyboard_event(const Cool::KeyboardEvent& event)
 {
-    if (event.action == GLFW_RELEASE) {
-        if (Input::matches_char("s", event.key) && event.mods.ctrl()) {
-            _exporter.image_export_window().open();
-        }
-        if (Input::matches_char("e", event.key) && event.mods.ctrl()) {
-            _exporter.video_export_window().open();
-        }
-    }
-    if (event.action == GLFW_PRESS || event.action == GLFW_REPEAT) {
-        if (Input::matches_char("z", event.key) && event.mods.ctrl()) {
-            ParametersHistory::get().move_backward();
-        }
-        if (Input::matches_char("y", event.key) && event.mods.ctrl()) {
-            ParametersHistory::get().move_forward();
-        }
-    }
+    DefaultApp::on_keyboard_event(event);
 }
 
 void App::on_mouse_button(const Cool::MouseButtonEvent<Cool::WindowCoordinates>& event)
 {
-    for (auto& view : _views) {
-        view.view.dispatch_mouse_button_event(view_event(event, view));
-    }
+    DefaultApp::on_mouse_button(event);
 }
 
 void App::on_mouse_scroll(const Cool::MouseScrollEvent<Cool::WindowCoordinates>& event)
 {
-    for (auto& view : _views) {
-        view.view.dispatch_mouse_scroll_event(view_event(event, view));
-    }
+    DefaultApp::on_mouse_scroll(event);
 }
 
 void App::on_mouse_move(const Cool::MouseMoveEvent<Cool::WindowCoordinates>& event)
 {
-    for (auto& view : _views) {
-        view.view.dispatch_mouse_move_event(view_event(event, view));
-    }
+    DefaultApp::on_mouse_move(event);
 }
--- src/App.h
+++ src/App.h
@@ -1,26 +1,16 @@
 #pragma once
 
-#include <Cool/AppManager/IApp.h>
-#include <Cool/Camera/CameraManager.h>
-#include <Cool/Exporter/Exporter.h>
-#include <Cool/Exporter/internal/Polaroid.h>
-#include <Cool/Gpu/FullscreenPipeline.h>
-#include <Cool/Image/ImageSizeConstraint.h>
-#include <Cool/Serialization/AutoSerializer.h>
-#include <Cool/Time/Clock_Realtime.h>
-#include <Cool/View/RenderableViewManager.h>
-#include <Cool/Window/WindowManager.h>
+#include <Cool/AppManager/DefaultApp.h>
 #include "ShaderManager/ShaderManagerManager.h"
 
 using namespace Cool;
 
-class App : public IApp {
+class App : public CoolDefault::DefaultApp {
 public:
     explicit App(Cool::WindowManager& windows);
 
     void update() override;
-    bool inputs_are_allowed() const override;
-    bool wants_to_show_menu_bar() const override;
+
     void imgui_windows() override;
     void imgui_menus() override;
 
@@ -30,31 +20,10 @@ public:
     void on_mouse_move(const Cool::MouseMoveEvent<Cool::WindowCoordinates>& event) override;
 
 private:
-    void           render(Cool::RenderTarget& render_target, float time);
-    Cool::Polaroid polaroid();
-    bool           aspect_ratio_is_constrained() const;
-    template<typename Event>
-    ViewEvent<Event> view_event(const Event& event, const RenderableView& view)
-    {
-        return {event,
-                _main_window.glfw(),
-                {view.render_target.current_size(),
-                 aspect_ratio_is_constrained()}};
-    }
+    void render(Cool::RenderTarget& render_target, float time);
 
 private:
-    Window&                     _main_window;
-    ShaderManagerManager        _shader_manager;
-    Cool::CameraManager         _camera;
-    Cool::Clock_Realtime        _clock;
-    Cool::ImageSizeConstraint   _preview_constraint;
-    Cool::RenderableViewManager _views; // Must be before the views because it is used to construct them
-    Cool::RenderableView&       _view;
-    Exporter                    _exporter;
-#ifdef DEBUG
-    bool _show_imgui_debug = true;
-    bool _show_imgui_demo  = false;
-#endif
+    ShaderManagerManager _shader_manager;
     // Must be declared last because its constructor modifies App, and its destructor requires all other members to still be alive
     Cool::AutoSerializer<App> _auto_serializer{Cool::File::root_dir() + "/last-session-cache.json", "App", *this};
 
@@ -64,7 +33,7 @@ private:
     template<class Archive>
     void serialize(Archive& archive)
     {
-        archive(cereal::make_nvp("Shader Manager Manager", _shader_manager),
-                cereal::make_nvp("Camera Manager", _camera));
+        archive(cereal::make_nvp("Shader Manager Manager", _shader_manager));
+        // cereal::make_nvp("Default App", reinterpret_cast<DefaultApp>(*this)));
     }
 };
