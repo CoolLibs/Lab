32c8e974d2f54436fbbfdeaf15faa9c8a564d91d
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-20 02:17:02 +0200

    [is0::CodeGen] Start

---
+++ src/is0/CodeGen.cpp
@@ -0,0 +1,25 @@
+#include "CodeGen.h"
+
+namespace CodeGen {
+
+std::string function_name(const FnNameParams& p)
+{
+    return std::string{p.node_template_name} + "_" + std::to_string(p.node_uuid);
+}
+
+std::string function_signature(const FnSignatureParams& p)
+{
+    return "float " + function_name(p.fn_name_params) + std::string{p.sdf_param_declaration};
+}
+
+std::string function_declaration(const FnSignatureParams& p)
+{
+    return function_signature(p) + ";";
+}
+
+std::string function_definition(const FnDefinitionParams& p)
+{
+    return function_signature(p.fn_signature_params); //
+}
+
+} // namespace CodeGen
---
+++ src/is0/CodeGen.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <Cool/Uuid/Uuid.h>
+
+namespace CodeGen {
+
+struct FnNameParams {
+    std::string_view node_template_name;
+    Cool::Uuid       node_uuid;
+};
+
+struct FnSignatureParams {
+    const FnNameParams& fn_name_params;
+    std::string_view    sdf_param_declaration;
+};
+
+struct FnDefinitionParams {
+    const FnSignatureParams& fn_signature_params;
+};
+
+std::string function_name(const FnNameParams& p);
+std::string function_signature(const FnSignatureParams& p);
+std::string function_declaration(const FnSignatureParams& p);
+std::string function_definition(const FnDefinitionParams& p);
+
+} // namespace CodeGen
+
+TEST_CASE("[is0::CodeGen] Function generation")
+{
+    // Given
+    const auto name             = std::string{"my_sdf"};
+    const auto name_params      = CodeGen::FnNameParams{name,
+                                                   Cool::Uuid{42}};
+    const auto signature_params = CodeGen::FnSignatureParams{name_params,
+                                                             "(vec3 pos)"};
+    // Then
+    CHECK(CodeGen::function_name(name_params) == "my_sdf_42");
+    CHECK(CodeGen::function_signature(signature_params) == "float my_sdf_42(vec3 pos)");
+    CHECK(CodeGen::function_declaration(signature_params) == "float my_sdf_42(vec3 pos);");
+}
