27760d2e918b4b3eb561ac73687d0eebe8e08d6e
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-25 16:47:58 +0200

    [is0::NodeEditor] Add find_input_node()

--- src/is0/NodeEditor.cpp
+++ src/is0/NodeEditor.cpp
@@ -57,6 +57,30 @@ static const Pin& find_pin(ed::PinId id, const std::vector<Node>& nodes)
     throw std::invalid_argument("pin not found");
 }
 
+static const Node& find_node_with_output_pin(ed::PinId pin_id, const std::vector<Node>& nodes)
+{
+    return *std::ranges::find_if(nodes, [&](const Node& node) {
+        return node.output_pin.id() == pin_id;
+    });
+}
+
+/**
+ * @brief Assumes that pin is an input pin and returns the node that is connected to it (or nullptr if there is none)
+ */
+static const Node* find_input_node(const Pin& pin, const std::vector<Node>& nodes, const std::vector<Link>& links)
+{
+    assert(pin.kind() == ed::PinKind::Input);
+    const auto link_it = std::ranges::find_if(links, [&](const Link& link) {
+        return link.to_pin_id == pin.id();
+    });
+    if (link_it == links.end()) {
+        return nullptr;
+    }
+    else {
+        return &find_node_with_output_pin(link_it->from_pin_id, nodes);
+    }
+}
+
 static void handle_link_creation(const std::vector<Node>& nodes, std::back_insert_iterator<std::vector<Link>> inserter)
 {
     if (ed::BeginCreate()) {
