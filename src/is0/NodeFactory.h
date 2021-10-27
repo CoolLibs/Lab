e433d920de41503da250bc2742f5b4081feee5f9
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-27 16:31:59 +0200

    [is0::NodeFactory] Update nodes

--- src/is0/NodeFactory.h
+++ src/is0/NodeFactory.h
@@ -26,7 +26,7 @@ public:
                                        .name          = "radius",
                                        .default_value = 1.f,
                                        .min_value     = 0.f,
-                                       .max_value     = 10.f}},
+                                       .max_value     = 100.f}},
                                    .sdf_identifiers        = {}});
         _node_templates.push_back({.name                   = "Repeat",
                                    .vec3_input_declaration = "(vec3 pos)",
@@ -34,6 +34,21 @@ public:
                                    .parameters             = {Cool::Parameter::FloatDesc{
                                        .name          = "cell_radius",
                                        .default_value = 5.f,
+                                       .min_value     = 0.001f,
+                                       .max_value     = 20.f}},
+                                   .sdf_identifiers        = {"SDF"}});
+        _node_templates.push_back({.name                   = "Twist",
+                                   .vec3_input_declaration = "(vec3 p)",
+                                   .code_template          = R"( 
+    float c = cos(k*p.y);
+    float s = sin(k*p.y);
+    mat2  m = mat2(c,-s,s,c);
+    vec3  q = vec3(m*p.xz,p.y);
+    return ${SDF}(q);
+	)",
+                                   .parameters             = {Cool::Parameter::FloatDesc{
+                                       .name          = "k",
+                                       .default_value = 10.f,
                                        .min_value     = 0.001f,
                                        .max_value     = 20.f}},
                                    .sdf_identifiers        = {"SDF"}});
