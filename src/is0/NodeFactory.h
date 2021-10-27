0c0421cfcf4e20cd23e274ae32b76759f7892291
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-27 03:18:13 +0200

    [is0::NodeFactory] Give better default values to the parameters of the nodes

--- src/is0/NodeFactory.h
+++ src/is0/NodeFactory.h
@@ -10,20 +10,32 @@ public:
         _node_templates.push_back({.name                   = "Sphere",
                                    .vec3_input_declaration = "(vec3 pos)",
                                    .code_template          = "    return length(pos) - radius;",
-                                   .parameters             = {Cool::Parameter::FloatDesc{"radius"}},
+                                   .parameters             = {Cool::Parameter::FloatDesc{
+                                       .name          = "radius",
+                                       .default_value = 1.f,
+                                       .min_value     = 0.f,
+                                       .max_value     = 10.f}},
                                    .sdf_identifiers        = {}});
         _node_templates.push_back({.name                   = "Cube",
                                    .vec3_input_declaration = "(vec3 pos)",
                                    .code_template          = R"( 
-	vec3 q = abs(pos) - 1.; 
+	vec3 q = abs(pos) - radius; 
 	return length(max(q, 0.)) + min(max(q.x, max(q.y, q.z)), 0.); 
 	)",
-                                   .parameters             = {},
+                                   .parameters             = {Cool::Parameter::FloatDesc{
+                                       .name          = "radius",
+                                       .default_value = 1.f,
+                                       .min_value     = 0.f,
+                                       .max_value     = 10.f}},
                                    .sdf_identifiers        = {}});
         _node_templates.push_back({.name                   = "Repeat",
                                    .vec3_input_declaration = "(vec3 pos)",
-                                   .code_template          = "    return ${SDF}((fract(pos / extent) - 0.5) * extent);",
-                                   .parameters             = {Cool::Parameter::FloatDesc{"extent"}},
+                                   .code_template          = "    return ${SDF}((fract(pos / cell_radius) - 0.5) * cell_radius);",
+                                   .parameters             = {Cool::Parameter::FloatDesc{
+                                       .name          = "cell_radius",
+                                       .default_value = 5.f,
+                                       .min_value     = 0.001f,
+                                       .max_value     = 20.f}},
                                    .sdf_identifiers        = {"SDF"}});
         _node_templates.push_back({.name                   = "Intersection",
                                    .vec3_input_declaration = "(vec3 pos)",
