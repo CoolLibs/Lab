884b9483ba01273542be84b426f549fee8ef5acf
Jules Fouchy <jules.fouchy@ntymail.com>
2021-11-02 00:33:23 +0100

    [is0::NodeTemplateParsing] Finish parse_node_template()

--- src/is0/NodeTemplateParsing.cpp
+++ src/is0/NodeTemplateParsing.cpp
@@ -2,12 +2,13 @@
 #include <Cool/Parameter/Parsing.h>
 #include <Cool/String/String.h>
 
-static std::string_view find_capture(std::string_view source)
+static std::string_view find_capture(std::string_view source, size_t* end_pos)
 {
     const auto capture_pos = Cool::String::find_matching_pair(source, '[', ']');
     if (!capture_pos.has_value()) {
         throw std::invalid_argument("Couldn't parse the capture group. It should be delimited by \"[ ]\"");
     }
+    *end_pos = capture_pos->second + 1;
     return source.substr(capture_pos->first + 1, capture_pos->second - capture_pos->first - 1);
 }
 
@@ -16,30 +17,39 @@ static std::vector<std::string> parse_capture(std::string_view source)
     return Cool::String::split_into_words(std::string{source});
 }
 
-static std::string find_sdf_input_declaration(std::string_view source)
+static std::string find_sdf_input_declaration(std::string_view source, size_t* end_pos)
 {
     const auto pos = Cool::String::find_matching_pair(source, '(', ')');
     if (!pos.has_value()) {
         throw std::invalid_argument("Couldn't parse the sdf input declaration. It should look like \"(vec3 pos)\"");
     }
+    *end_pos = pos->second + 1;
     return std::string{source.substr(pos->first, pos->second - pos->first + 1)};
 }
 
-static std::string find_sdf_body(std::string_view source)
+static std::string find_sdf_body(std::string_view source, size_t* end_pos)
 {
     const auto pos = Cool::String::find_matching_pair(source, '{', '}');
     if (!pos.has_value()) {
         throw std::invalid_argument("Couldn't parse the sdf body. It should be delimited by \"{ }\"");
     }
+    *end_pos = pos->second + 1;
     return std::string{source.substr(pos->first, pos->second - pos->first + 1)};
 }
 
 void parse_node_template(NodeTemplate& node_template, std::string_view source)
 {
-    node_template.sdf_identifiers        = parse_capture(find_capture(source));
-    node_template.vec3_input_declaration = find_sdf_input_declaration(source);
-    node_template.code_template          = find_sdf_body(source);
-    node_template.parameters             = Cool::parse_all_parameter_desc(source);
+    size_t     next_pos;
+    const auto trim_source = [&]() {
+        source = source.substr(next_pos, source.length() - next_pos);
+    };
+    node_template.sdf_identifiers = parse_capture(find_capture(source, &next_pos));
+    trim_source();
+    node_template.vec3_input_declaration = find_sdf_input_declaration(source, &next_pos);
+    trim_source();
+    node_template.code_template = find_sdf_body(source, &next_pos);
+    trim_source();
+    node_template.parameters = Cool::parse_all_parameter_desc(source);
 }
 
 TEST_CASE("[is0::NodeParsing] parse_node_template()")
@@ -51,9 +61,9 @@ TEST_CASE("[is0::NodeParsing] parse_node_template()")
 }
 float {
     "Name": "radius",
-    "Default value": 1.0,
-    "Min value": 0.0,
-    "Max value": 100.0
+    "Default": 1.0,
+    "Min": 0.0,
+    "Max": 100.0
 }
 )";
     // When
@@ -67,7 +77,7 @@ float {
     const std::vector<std::string> expected_sdf_identifiers = {"MY_SDF1", "MY_SDF2"};
     CHECK(node_template.sdf_identifiers == expected_sdf_identifiers);
     REQUIRE(node_template.parameters.size() == 1);
-    CHECK(std::holds_alternative<Cool::Parameter::FloatDesc>(node_template.parameters[1]));
+    CHECK(std::holds_alternative<Cool::Parameter::FloatDesc>(node_template.parameters[0]));
 }
 
 TEST_CASE("[is0::NodeParsing] find_capture()")
@@ -77,5 +87,6 @@ TEST_CASE("[is0::NodeParsing] find_capture()")
     return max(${MY_SDF1}(weird_pos), ${MY_SDF2}(weird_pos));
 }
 )";
-    CHECK(find_capture(source) == "MY_SDF1 MY_SDF2");
+    size_t            aaa;
+    CHECK(find_capture(source, &aaa) == "MY_SDF1 MY_SDF2");
 }
