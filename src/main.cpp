922d929ea54a60726c365beb2e5b54a7e4913524
Jules Fouchy <jules.fouchy@ntymail.com>
2021-11-03 16:03:36 +0100

    [main] Only maximize on startup up if we are building in release

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,5 @@ int main()
 {
     CoolDefault::init();
     Cool::run<App>({Cool::WindowConfig{.title                  = "Cool Lab",
-                                       .maximize_on_startup_if = true}});
+                                       .maximize_on_startup_if = Cool::is_building_as(Cool::BuildMode::Release)}});
 }
