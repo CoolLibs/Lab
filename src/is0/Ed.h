6d5f1d5c41eaa4711fb87e5aad2b5bfb91fcdfc0
Jules Fouchy <jules.fouchy@ntymail.com>
2021-11-05 00:19:35 +0100

    Remove imgui-node-editor

--- src/is0/Ed.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <imgui-node-editor/imgui_node_editor.h>
-namespace ed = ax::NodeEditor;
--- src/is0/EdUniqueContext.h
+++ src/is0/EdUniqueContext.h
@@ -1,45 +1,33 @@
 #pragma once
 
 #include <imnodes/imnodes.h>
-#include "Ed.h"
 
 class EdUniqueContext {
 public:
     EdUniqueContext()
-        : _context{ed::CreateEditor()}
     {
         ImNodes::CreateContext();
         ImNodes::LoadCurrentEditorStateFromIniFile("is0_node_editor.ini");
     }
     ~EdUniqueContext()
     {
-        ImNodes::SaveCurrentEditorStateToIniFile("is0_node_editor.ini");
-        ImNodes::DestroyContext();
-        if (_context) {
-            ed::DestroyEditor(_context);
+        if (!_was_moved) {
+            ImNodes::SaveCurrentEditorStateToIniFile("is0_node_editor.ini");
+            ImNodes::DestroyContext();
         }
     }
     EdUniqueContext(const EdUniqueContext&) = delete;
     EdUniqueContext& operator=(const EdUniqueContext&) = delete;
     EdUniqueContext(EdUniqueContext&& rhs) noexcept
-        : _context{rhs._context}
     {
-        rhs._context = nullptr;
+        rhs._was_moved = true;
     };
     EdUniqueContext& operator=(EdUniqueContext&& rhs) noexcept
     {
-        if (this != &rhs) {
-            _context     = rhs._context;
-            rhs._context = nullptr;
-        }
+        rhs._was_moved = true;
         return *this;
     }
 
-    ed::EditorContext&       operator*() { return *_context; }
-    ed::EditorContext*       operator->() { return _context; }
-    const ed::EditorContext& operator*() const { return *_context; }
-    const ed::EditorContext* operator->() const { return _context; }
-
 private:
-    ed::EditorContext* _context;
+    bool _was_moved = false;
 };
