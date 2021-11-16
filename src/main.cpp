389bf3e5f1bb4b7dab612b6f77c566d632b68057
Jules Fouchy <jules.fouchy@ntymail.com>
2021-11-16 16:14:08 +0100

    [Cool] Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <Cool/Core/run.h>
-#include <Cool/Default/DefaultInit.h>
 #include "App.h"
 
 int main()
 {
-    CoolDefault::init();
     Cool::run<App>({Cool::WindowConfig{.title                  = "Cool Lab",
                                        .maximize_on_startup_if = Cool::is_building_as(Cool::BuildMode::Release)}});
 }
