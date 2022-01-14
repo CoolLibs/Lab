79f9ffdfd39f1f6c1f9a6a4694c0db8aeab64fed
Emma <emma.pernin1@gmail.com>
2022-01-14 11:35:03 +0100

    [Shaders] Add reflection shader

---
+++ is0 shaders/reflection.glsl
@@ -0,0 +1,37 @@
+vec3 reflet_out    = reflect(rd, normal);
+vec3 refraction_in = refract(rd, normal, 1. / IOR);
+
+vec3        p_enter = p - normal * SURF_DIST * 3.;
+RayMarchRes d_in    = rayMarching(p_enter, refraction_in, INVERSED_SDF); // Inside
+float       d2      = d_in.dist;
+
+vec3  p_exit         = p_enter + refraction_in * d2;
+vec3  norm_exit      = -getNormal(p_exit);
+vec3  reflect_text   = vec3(0);
+vec3  refraction_out = vec3(0);
+float abb            = .01;
+// Red
+refraction_out = refract(refraction_in, norm_exit, IOR - abb);
+if (dot(refraction_out, refraction_out) == 0.) {
+    refraction_out = reflect(refraction_in, norm_exit);
+}
+reflect_text.r = refraction_out.r;
+// Green
+refraction_out = refract(refraction_in, norm_exit, IOR);
+if (dot(refraction_out, refraction_out) == 0.) {
+    refraction_out = reflect(refraction_in, norm_exit);
+}
+reflect_text.g = refraction_out.g;
+// Blue
+refraction_out = refract(refraction_in, norm_exit, IOR + abb);
+if (dot(refraction_out, refraction_out) == 0.) {
+    refraction_out = reflect(refraction_in, norm_exit);
+}
+reflect_text.b = refraction_out.b;
+
+float density            = .1;
+float optical_distance   = exp(-d2 * density);
+reflect_text             = reflect_text * optical_distance;
+float fresnel_reflection = pow(1. + dot(rd, normal), 5.);
+
+finalCol += mix(reflect_text, reflet_out, fresnel_reflection);
--- src/is0/RenderEffect_Reflection.cpp
+++ src/is0/RenderEffect_Reflection.cpp
@@ -9,45 +9,7 @@ std::string code_gen_reflection_parameters(const RenderEffect_Reflection& reflec
 
 std::string code_gen_reflection()
 {
-    return R"(
-        vec3 refletOut = reflect(rd, normal);
-        vec3 refractionIn = refract(rd,normal,1./IOR);
-
-        vec3 pEnter = p - normal * SURF_DIST * 3.;
-        RayMarchRes dIn = rayMarching(pEnter, refractionIn, INVERSED_SDF); // Inside
-        float d2 = dIn.dist;
-
-        vec3 pExit = pEnter + refractionIn * d2;
-        vec3 normExit = -getNormal(pExit);
-        vec3 reflectText = vec3(0);
-        vec3 refractionOut = vec3(0);
-        float abb = .01;
-        //Red
-        refractionOut = refract(refractionIn, normExit, IOR-abb);
-        if(dot(refractionOut, refractionOut) == 0.){
-            refractionOut = reflect(refractionIn, normExit);
-        }
-        reflectText.r = refractionOut.r;
-        //Green
-        refractionOut = refract(refractionIn, normExit, IOR);
-        if(dot(refractionOut, refractionOut) == 0.){
-            refractionOut = reflect(refractionIn, normExit);
-        }
-        reflectText.g = refractionOut.g;
-        //Blue
-        refractionOut = refract(refractionIn, normExit, IOR+abb);
-        if(dot(refractionOut, refractionOut) == 0.){
-            refractionOut = reflect(refractionIn, normExit);
-        }
-        reflectText.b = refractionOut.b;
-        
-        float dens = .1;
-        float optDist = exp(-d2 * dens);
-        reflectText= reflectText * optDist;
-        float fresnelRefl = pow(1. + dot(rd, normal), 5.);
-
-        finalCol += mix(reflectText, refletOut, fresnelRefl);
-    )";
+    return "#include \"is0 shaders/reflection.glsl\"\n";
 };
 
 bool reflection_imgui(RenderEffect_Reflection& reflection)
