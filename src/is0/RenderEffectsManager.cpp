78de03f5fbb4bf0d0650540cb2c6807892d8730c
Emma <emma.pernin1@gmail.com>
2022-01-17 15:59:58 +0100

    [RenderEffects General] Glow et Reflection ok

--- src/is0/RenderEffectsManager.cpp
+++ src/is0/RenderEffectsManager.cpp
@@ -11,29 +11,44 @@ RenderEffectsManager::RenderEffectsManager(std::string_view render_effects_folde
     _render_effects = load_effect(_render_effects_folder_path);
 }
 
-std::vector<RenderEffect> load_effect(std::string_view render_effects_folder_path)
+RenderEffectsGestion load_effect(std::string_view render_effects_folder_path)
 {
-    std::vector<RenderEffect> render_effects;
-    for (const auto& file : std::filesystem::directory_iterator{render_effects_folder_path}) {
-        if (file.is_regular_file()) {
-            try {
-                RenderEffect render_effect;
-                render_effect.name = file.path().stem().string();
-                parse_render_effect(render_effect, Cool::File::to_string(file.path().string()));
-                render_effects.push_back(render_effect);
-            }
-            catch (const std::exception& e) {
-                Cool::Log::ToUser::warn("is0::RenderEffectsManager::" + file.path().stem().string(), "Failed to parse node from file '{}':\n{}", file.path().string(), e.what());
+    RenderEffectsGestion effects_gestion;
+    effects_gestion._render_effects_objects.reserve(100);
+    effects_gestion._render_effects_world.reserve(100);
+    for (const auto& entry : std::filesystem::directory_iterator{render_effects_folder_path}) {
+        if (entry.is_directory()) {
+            std::vector<RenderEffect>& effects = entry.path().stem() == "Objects"
+                                                     ? effects_gestion._render_effects_objects
+                                                     : effects_gestion._render_effects_world;
+            for (const auto& file : std::filesystem::directory_iterator{entry.path()}) {
+                if (file.is_regular_file()) {
+                    try {
+                        RenderEffect render_effect;
+                        render_effect.name = file.path().stem().string();
+                        parse_render_effect(render_effect, Cool::File::to_string(file.path().string()));
+                        effects.push_back(render_effect);
+                    }
+                    catch (const std::exception& e) {
+                        Cool::Log::ToUser::warn("is0::RenderEffectsManager::" + file.path().stem().string(), "Failed to parse node from file '{}':\n{}", file.path().string(), e.what());
+                    }
+                }
             }
         }
     }
-    return render_effects;
+    return effects_gestion;
 };
 
 std::string code_gen_effects_parameters(const RenderEffectsManager& effects)
 {
     std::string code = "";
-    for (const auto& effect : effects._render_effects) {
+    for (const auto& effect : effects._render_effects._render_effects_objects) {
+        if (effect.is_active) {
+            code += CodeGen::parameters_definitions(effect.parameters);
+            code += "\n";
+        }
+    }
+    for (const auto& effect : effects._render_effects._render_effects_world) {
         if (effect.is_active) {
             code += CodeGen::parameters_definitions(effect.parameters);
             code += "\n";
@@ -45,7 +60,7 @@ std::string code_gen_effects_parameters(const RenderEffectsManager& effects)
 std::string code_gen_effects_object(const RenderEffectsManager& effects)
 {
     std::string code = "";
-    for (const auto& effect : effects._render_effects) {
+    for (const auto& effect : effects._render_effects._render_effects_objects) {
         if (effect.is_active) {
             code += effect.code_template;
             code += "\n";
@@ -56,16 +71,24 @@ std::string code_gen_effects_object(const RenderEffectsManager& effects)
 
 std::string code_gen_effects_world(const RenderEffectsManager& effects)
 {
-    return "";
+    std::string code = "";
+    for (const auto& effect : effects._render_effects._render_effects_world) {
+        if (effect.is_active) {
+            code += effect.code_template;
+            code += "\n";
+        }
+    }
+    return code;
 };
 
 bool effect_imgui(RenderEffect& effect)
 {
     ImGui::Text(effect.name.c_str());
     bool has_changed = false;
+    ImGui::PushID(&effect);
     effect.parameters.imgui([&has_changed]() { has_changed |= true; });
     has_changed |= ImGui::Checkbox("Enabled", &effect.is_active);
-    Cool::Log::info(effect.is_active);
+    ImGui::PopID();
     return has_changed;
 }
 
@@ -73,7 +96,11 @@ bool effect_imgui_window(RenderEffectsManager& effects)
 {
     bool has_changed = false;
     ImGui::Begin("Shading");
-    for (auto& param : effects._render_effects) {
+    for (auto& param : effects._render_effects._render_effects_objects) {
+        has_changed |= effect_imgui(param);
+        ImGui::Separator();
+    }
+    for (auto& param : effects._render_effects._render_effects_world) {
         has_changed |= effect_imgui(param);
         ImGui::Separator();
     }
