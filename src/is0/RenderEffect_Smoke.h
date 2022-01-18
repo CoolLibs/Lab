cc9f262589ca6256ea9b93a63277fa28bc4c5b3f
Jules Fouchy <jules.fouchy@ntymail.com>
2022-01-18 10:02:17 +0100

    [is0::RenderEffect_Smoke] Fix warnings

--- src/is0/RenderEffect_Smoke.h
+++ src/is0/RenderEffect_Smoke.h
@@ -4,21 +4,21 @@
 
 struct RenderEffect_Smoke {
     Cool::Parameter::Float ABSORPTION_COEFFICIENT{{.name          = "Absorption Coefficient",
-                                                   .default_value = 2.5,
-                                                   .min_value     = 0.01,
-                                                   .max_value     = 5.}};
+                                                   .default_value = 2.5f,
+                                                   .min_value     = 0.01f,
+                                                   .max_value     = 5.f}};
 
     // Reduce the following values to enhance performance
 
     Cool::Parameter::Float MARCH_MULTIPLIER{{.name          = "March Multiplier",
-                                             .default_value = 0.5,
-                                             .min_value     = 0.01,
-                                             .max_value     = 3.}};
+                                             .default_value = 0.5f,
+                                             .min_value     = 0.01f,
+                                             .max_value     = 3.f}};
 
     Cool::Parameter::Float MAX_VOLUME_MARCH_STEPS{{.name          = "Max Volume March Steps",
-                                                   .default_value = 50.0,
-                                                   .min_value     = 20.0,
-                                                   .max_value     = 75.0}};
+                                                   .default_value = 50.f,
+                                                   .min_value     = 20.f,
+                                                   .max_value     = 75.f}};
 
     bool is_active = true;
 };
