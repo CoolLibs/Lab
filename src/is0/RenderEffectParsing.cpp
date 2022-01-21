3e7e20c55d84d7f2948019c49bb8e188e8163a11
Jolan <goulin.jolan@gmail.com>
2022-01-21 11:17:56 +0100

    Bug correcting

--- src/is0/RenderEffectParsing.cpp
+++ src/is0/RenderEffectParsing.cpp
@@ -13,5 +13,5 @@ void parse_render_effect(RenderEffect& render_effect, std::string_view source)
     trim_source();
     render_effect.code = find_body(source, &next_pos);
     trim_source();
-    render_effect.parameters = Cool::ParameterList::ParameterList(Cool::parse_all_parameter_desc(source));
+    render_effect.parameters = Cool::ParameterList(Cool::parse_all_parameter_desc(source));
 }
