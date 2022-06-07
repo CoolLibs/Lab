d686e798b6c7f895d5542659938d015b1ae7fbe4
Jules Fouchy <jules.fouchy@ntymail.com>
2022-06-07 23:04:29 +0200

    🔥 [ShaderManagerManager]

--- src/ShaderManager/ShaderManagerManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "ShaderManagerManager.h"
-
-void ShaderManagerManager::imgui_windows()
-{
-    ImGui::Begin("Shader Style");
-    ImGui::Checkbox("Use nodes", &_use_nodes);
-    ImGui::End();
-    operator*().imgui_windows();
-}
--- src/ShaderManager/ShaderManager_FromText.cpp
+++
@@ -1,7 +0,0 @@
-#include <Cool/Camera/Camera.h>
-#include <Cool/File/File.h>
-#include <Cool/Log/ToUser.h>
-#include <Cool/Parameter/ParameterU.h>
-#include <Cool/Parameter/set_uniform.h>
-#include <Cool/Path/Path.h>
-#include <fstream>
