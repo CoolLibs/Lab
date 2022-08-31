1c94193725fed5feaa9f73ad184cdf9bef502633
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-31 11:08:19 +0200

    ♻️ [shapness_alternative] Add .glsl

---
+++ res/shader-lib/sharpness_alternative.glsl
@@ -0,0 +1,39 @@
+
+// from https://www.shadertoy.com/view/llXGWf
+
+// Sharpness kernel
+// -x   -x    -x
+// -x  x*8+1  -x
+// -x   -x    -x
+
+vec3 sharpness_alternative(
+    vec2 in_uv, float effect_intensity,
+    float Offset_strength, float Sharpening_strength
+
+)
+{
+    vec3 out_color = image(in_uv);
+    vec3 blur      = out_color;
+    blur += image(in_uv + vec2(0.0, 0.001 * Offset_strength));
+    blur += image(in_uv + vec2(0.001 * Offset_strength, 0.0));
+    blur += image(in_uv - vec2(0.0, 0.001 * Offset_strength));
+    blur += image(in_uv - vec2(0.001 * Offset_strength, 0.0));
+
+    blur += image(in_uv + vec2(0.001 * Offset_strength)) / 2.0;
+    blur += image(in_uv - vec2(0.001 * Offset_strength)) / 2.0;
+    blur += image(in_uv + vec2(0.001 * Offset_strength, -0.001 * Offset_strength)) / 2.0;
+    blur += image(in_uv + vec2(-0.001 * Offset_strength, 0.001 * Offset_strength)) / 2.0;
+
+    blur /= 7.0;
+
+    float sharpness = (out_color - blur).r * Sharpening_strength;
+
+    // out_color = vec3(0);
+    // out_color -= step(distance(fract(uv.x + (sin(_time) + 1.0) / 2.0) + 0.5, 0.5) * 2.0, 0.013);
+
+    if (floor(in_uv.x + ((sin(_time) + 1.0) / 2.0)) > 0.0)
+    {
+        out_color += sharpness;
+    }
+    return out_color;
+}
