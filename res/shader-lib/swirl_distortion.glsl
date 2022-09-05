923e8367473323e11896657a48ef8f2f103b4038
TaliaKah <anaissaratagougne@tutanota.com>
2022-09-05 13:39:04 +0200

    🐛 [swirl_distortion] Do not use _aspect_ratio

--- res/shader-lib/swirl_distortion.glsl
+++ res/shader-lib/swirl_distortion.glsl
@@ -1,13 +1,13 @@
 // https://www.shadertoy.com/view/Xscyzn
 
 vec2 swirl_distortion(
-    vec2 in_uv, float effect_intensity, float aspect_ratio,
+    vec2 in_uv, float effect_intensity,
     vec2 center, float srength, float size
 )
 {
     vec2 uv = in_uv - center;
 
-    float len    = length(uv * vec2(aspect_ratio, 1.));
+    float len    = length(uv);
     float angle  = atan(uv.y, uv.x) + srength * effect_intensity * TAU * smoothstep(size, 0., len);
     float radius = length(uv);
 
