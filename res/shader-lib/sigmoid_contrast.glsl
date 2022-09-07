63f2928691d0adea96b2f69fe827b873405661d2
TaliaKah <anaissaratagougne@tutanota.com>
2022-09-07 18:20:13 +0200

    🐛 [sigmoid_contrast] Fixed

--- res/shader-lib/sigmoid_contrast.glsl
+++ res/shader-lib/sigmoid_contrast.glsl
@@ -1,8 +1,8 @@
 // https://www.shadertoy.com/view/MtX3z2
 
-float Sigmoid(float x)
+float Sigmoid(float x, float strength)
 {
-    return 1.0 / (1.0 + (exp(-(x - 0.5) * Strength)));
+    return 1.0 / (1.0 + (exp(-(x - 0.5) * strength)));
 }
 
 vec3 sigmoid_contrast(
@@ -13,10 +13,10 @@ vec3 sigmoid_contrast(
     return mix(
         in_color,
         vec3(
-            Sigmoid(in_color.r),
-            Sigmoid(in_color.g),
-            Sigmoid(in_color.b)
+            Sigmoid(in_color.r, strength),
+            Sigmoid(in_color.g, strength),
+            Sigmoid(in_color.b, strength)
         ),
         effect_intensity
-    )
+    );
 }
