5598384ecf2adaa29be8bd940e4418e3f5331f31
Jules Fouchy <jules.fouchy@ntymail.com>
2022-11-22 00:23:41 +0100

    🚨 Fix warning

--- src/Module_Nodes/input_to_primitive_type.cpp
+++ src/Module_Nodes/input_to_primitive_type.cpp
@@ -4,7 +4,10 @@ namespace Lab {
 
 auto input_to_primitive_type(Cool::AnyInput const& input) -> std::optional<PrimitiveType>
 {
+    {
 #include "generated/input_to_primitive_type.inl"
+    } // Braces are here to silence a "misleading indent" warning
+
     return std::nullopt;
 }
 
