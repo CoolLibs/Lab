98902c11ccbaa7aed3efc255afc568ced63a4724
Jules Fouchy <jules.fouchy@ntymail.com>
2023-01-13 15:25:27 +0100

    ✨ [Icon] Icon in COOL

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <Cool/Core/run.h>
-#include <filesystem>
 #include "App.h"
 
 int main()
 {
     Cool::run<Lab::App>({Cool::WindowConfig{
         .title                  = "CoolLab",
-        .icon_path              = "windows-app-ressources/lab.png",
         .maximize_on_startup_if = true,
     }});
 }
