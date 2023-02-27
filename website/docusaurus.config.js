98e67bf0d8680873649960f6dd48131ddfec068d
Jules Fouchy <jules.fouchy@ntymail.com>
2023-02-27 17:31:58 +0100

    🌐 Update config for custom domain name

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -17,10 +17,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: `https://${organizationName}.github.io`,
+  url: `https://coollab-art.com/`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/${projectName}/`,
+  baseUrl: `/`,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
