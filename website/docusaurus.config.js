399bf6f4192e413917a64d315ffc5592e7d18a1c
Jules Fouchy <jules.fouchy@ntymail.com>
2023-03-13 21:01:49 +0100

    🌐 [website] Dark mode by default

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: false,
-        respectPrefersColorScheme: true,
+        respectPrefersColorScheme: false,
       },
       navbar: {
         title: '',
