a7adbf43af74f412580456ecdd6622f6298cd0c3
Jules Fouchy <jules.fouchy@ntymail.com>
2023-03-21 02:34:41 +0100

    🌐 [website] Try loading images for the gallery

--- website/src/pages/Gallery.js
+++ website/src/pages/Gallery.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import Layout from '@theme/Layout';
+import React from "react"
+import Layout from "@theme/Layout"
+import BrowserOnly from "@docusaurus/BrowserOnly"
 
-export default function Gallery() {
+import loadable from "@loadable/component"
+const Impl = loadable(() => import("../components/GalleryImpl"))
+
+export default function () {
   return (
     <Layout title="Hello" description="Hello React Page">
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50vh',
-          fontSize: '20px',
-        }}>
-        <p>
-            🚧 COMING SOON 🚧
-        </p>
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "50vh",
+          fontSize: "20px",
+        }}
+      >
+        <p>🚧 COMING SOON 🚧</p>
+        <BrowserOnly>{() => <Impl />}</BrowserOnly>
       </div>
     </Layout>
-  );
+  )
 }
