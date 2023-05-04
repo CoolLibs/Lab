fa165449b75e2e91a54075f369627e2e9d36c9e5
Jules Fouchy <jules.fouchy@ntymail.com>
2023-05-04 15:33:27 +0200

    🌐 [website] Force refresh favicon

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -14,7 +14,7 @@ const projectName = "Lab"
 const config = {
   title: "Coollab",
   tagline: "A free generative-art software",
-  favicon: "img/favicon.ico",
+  favicon: "img/favicon2.ico",
 
   // Set the production url of your site here
   url: `https://coollab-art.com/`,
