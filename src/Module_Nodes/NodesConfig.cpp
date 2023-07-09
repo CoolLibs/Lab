f9ac11a0c670fd350b9ae193657154a71dcbc9d5
Jules Fouchy <jules.fouchy@ntymail.com>
2023-07-09 15:27:50 +0200

    🧼 [Nodes] Improve previous commit code

--- src/Module_Nodes/NodesConfig.cpp
+++ src/Module_Nodes/NodesConfig.cpp
@@ -107,14 +107,10 @@ void NodesConfig::main_node_toggle(Cool::NodeId const& node_id)
     bool       is_main  = was_main;
     if (Cool::ImGuiExtras::toggle("Main node", &is_main))
     {
-        if (is_main && !was_main)
-        {
+        if (is_main)
             set_main_node_id(node_id);
-        }
-        else if (!is_main && was_main)
-        {
+        else
             set_main_node_id({}); // Unselect main node
-        }
     }
 }
 
