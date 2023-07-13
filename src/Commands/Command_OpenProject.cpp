1ea0611841b7c6e46d7f457b7c78f801ce06cf77
Jules Fouchy <jules.fouchy@ntymail.com>
2023-07-13 13:23:35 +0200

    🧼 [Project] Split Command_OpenProject::execute() into several functions

--- src/Commands/Command_OpenProject.cpp
+++ src/Commands/Command_OpenProject.cpp
@@ -1,31 +1,14 @@
 #include <Commands/Command_OpenProject.h>
 #include "CommandCore/LAB_REGISTER_COMMAND.h"
+#include "Cool/Log/OptionalErrorMessage.h"
 #include "Cool/Serialization/Serialization.h"
 #include "Project.h"
 #include "cereal/archives/json.hpp"
 
 namespace Lab {
 
-void Command_OpenProject::execute(CommandExecutionContext_Ref const& ctx) const
+static void set_window_title(CommandExecutionContext_Ref const& ctx, std::filesystem::path const& path)
 {
-    auto const error = Cool::Serialization::load<Project, cereal::JSONInputArchive>(ctx.project(), path);
-    if (error)
-    {
-        error.send_error_if_any(
-            [&](std::string const& message) {
-                return Cool::Message{
-                    .category = "Loading Project",
-                    .message  = message,
-                    .severity = Cool::MessageSeverity::Warning,
-                };
-            },
-            Cool::Log::ToUser::console()
-        );
-        ctx.project_path() = "";
-        // TODO(Project) Load a default project, otherwise we could probably have corrupted state if only part of the project was loaded.
-        return;
-    }
-
     glfwSetWindowTitle(
         ctx.main_window().glfw(),
         fmt::format(
@@ -34,7 +17,34 @@ void Command_OpenProject::execute(CommandExecutionContext_Ref const& ctx) const
         )
             .c_str()
     );
+}
 
+static void send_error_message(Cool::OptionalErrorMessage const& error)
+{
+    error.send_error_if_any(
+        [&](std::string const& message) {
+            return Cool::Message{
+                .category = "Loading Project",
+                .message  = message,
+                .severity = Cool::MessageSeverity::Warning,
+            };
+        },
+        Cool::Log::ToUser::console()
+    );
+}
+
+void Command_OpenProject::execute(CommandExecutionContext_Ref const& ctx) const
+{
+    auto const error = Cool::Serialization::load<Project, cereal::JSONInputArchive>(ctx.project(), path);
+    if (error)
+    {
+        send_error_message(error);
+        ctx.project_path() = "";
+        // TODO(Project) Load a default project, otherwise we could probably have corrupted state if only part of the project was loaded.
+        return;
+    }
+
+    set_window_title(ctx, path);
     ctx.project().is_first_frame = true;
     ctx.project_path()           = path; // Only assign path if loading was successful.
 }
