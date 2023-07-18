a0d4c14a1def943c7eb222185e35e358b479c17a
Jules Fouchy <jules.fouchy@ntymail.com>
2023-07-18 14:02:35 +0200

    💡 [Project] Add TODO

--- src/ProjectManager/Command_SaveProject.cpp
+++ src/ProjectManager/Command_SaveProject.cpp
@@ -7,7 +7,7 @@ namespace Lab {
 void Command_SaveProject::execute(CommandExecutionContext_Ref const& ctx) const
 {
     if (!ctx.project_path())
-        return;
+        return; // TODO(Project) We would to still want to save it on the side, for autosave, so that we can restore it if there is a crash.
     // TODO(Project) If we are on the default project, run a Save As ? (ask the user where they want to save their project) But this will be annoying with the autosave (-> we have to add a bool in this command to decide if it should trigger a Save As or not).
     save_project_to(ctx, *ctx.project_path()); // TODO(Project) handle error from saving
 }
