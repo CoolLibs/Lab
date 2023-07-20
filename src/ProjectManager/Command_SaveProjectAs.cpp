8a8ef00e9a8ad6d58936fab007660294b957e661
Jules Fouchy <jules.fouchy@ntymail.com>
2023-07-20 23:38:31 +0200

    ✨ [Project] SaveAs: Only change the path if we were on the unsaved project

--- src/ProjectManager/Command_SaveProjectAs.cpp
+++ src/ProjectManager/Command_SaveProjectAs.cpp
@@ -13,7 +13,8 @@ void Command_SaveProjectAs::execute(CommandExecutionContext_Ref const& ctx) cons
         dialog_to_save_project_as(ctx.command_executor()); // Save failed, try in another location.
         return;
     }
-    set_current_project_path(ctx, path); // TODO(Project) Only change the path if we were on the default project ? I think the most common use case is: you are working on your project, then want to save a specific step, so you run as Save As. But then you want to keep working on the main project, not the step that you saved.
+    if (!ctx.project_path().has_value()) // Only change the path if we were on the unsaved project. The most common use case is: you are working on your project, then want to save a specific step, so you run a Save As. But then you want to keep working on the main project, not on the step that you saved.
+        set_current_project_path(ctx, path);
 }
 
 [[nodiscard]] auto Command_SaveProjectAs::to_string() const -> std::string
