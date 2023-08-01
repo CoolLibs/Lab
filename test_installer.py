b0c8dd70b112cd3c7ca958f9c60fbd66a44e1152
Jules Fouchy <jules.fouchy@ntymail.com>
2023-08-02 01:15:50 +0200

    🔧 [Installer] Final NSIS config

--- test_installer.py
+++ test_installer.py
@@ -4,12 +4,6 @@ import os
 def main():
     from pathlib import Path
 
-    # os.system(
-    #     f"cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -SC:/Dev/Cool/Lab2 -Bc:/Dev/Cool/Lab2/build -G Ninja"
-    # )
-    # os.system(
-    #     "cmake --build c:/Dev/Cool/Lab2/build --config Release --target Coollab --"
-    # )
     os.chdir(Path(__file__).parent.joinpath("build"))
     os.system("cpack")
     os.system('"Coollab-beta-8 WIP-Windows.exe"')
