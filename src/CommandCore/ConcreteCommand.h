7c2650aa86a29c050fe8ef3d21232c0377bfff7d
Jules Fouchy <jules.fouchy@ntymail.com>
2023-09-24 13:32:02 +0200

    🎨 Apply clang-format

--- src/CommandCore/ConcreteCommand.h
+++ src/CommandCore/ConcreteCommand.h
@@ -7,20 +7,20 @@ class CommandExecutionContext_Ref;
 
 template<typename T>
 concept ConcreteCommand = requires(const T& a, const T& b, CommandExecutionContext_Ref& command_execution_context, const MakeReversibleCommandContext_Ref& make_reversible_command_context) {
-    // Has an execute() method
-    a.execute(command_execution_context);
-    // Has a to_string() method
-    {
-        a.to_string()
-    } -> std::convertible_to<std::string>;
+                              // Has an execute() method
+                              a.execute(command_execution_context);
+                              // Has a to_string() method
+                              {
+                                  a.to_string()
+                                  } -> std::convertible_to<std::string>;
 
-    // TODO require a serialization function
-};
+                              // TODO require a serialization function
+                          };
 
 template<typename T>
 concept ConcreteCommandThatCanBeMadeReversible = ConcreteCommand<T> && requires(const T& a, const MakeReversibleCommandContext_Ref& make_reversible_command_context) {
-    // Has a make_reversible() method
-    a.make_reversible(make_reversible_command_context);
-};
+                                                                           // Has a make_reversible() method
+                                                                           a.make_reversible(make_reversible_command_context);
+                                                                       };
 
 } // namespace Lab
