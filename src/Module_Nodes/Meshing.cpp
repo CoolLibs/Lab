c86d8637da933a633aba57fcf5951379f2c8a058
dsmtE <desmet.enguerrand@gmail.com>
2023-09-27 00:59:50 +0200

    ✨[Meshing] add meshing file

---
+++ src/Module_Nodes/Meshing.cpp
@@ -0,0 +1,13 @@
+#include <glpp/Functions/Texture.h>
+#include <glpp/Functions/Shader.h>
+#include <glpp/UniqueHandles/UniqueTexture.h>
+#include <Cool/Gpu/OpenGL/SSBO.h>
+#include <Cool/Gpu/OpenGL/ComputeShader.h>
+
+namespace Lab {
+
+void meshing_test(std::string const& shader_code) {
+    Cool::Log::Debug::info("Meshing", "test");
+}
+
+}  // namespace Lab
---
+++ src/Module_Nodes/Meshing.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Lab {
+
+void meshing_test(std::string const& shader_code);
+
+} // namespace Lab
