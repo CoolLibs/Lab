79c7977376b0bc30c109ed307877b2bc329fd9e4
Jules Fouchy <jules.fouchy@ntymail.com>
2023-12-03 19:55:16 +0100

    ♻️ [Particles] Move ParticleSystem code into Module_Particles

--- src/Module_Particles/Module_Particles.cpp
+++ src/Module_Particles/Module_Particles.cpp
@@ -1,11 +1,42 @@
 #include "Module_Particles.h"
-#include <string>
+#include "Cool/File/File.h"
+#include "Cool/Particles/ParticleSystem.h"
 
 namespace Lab {
 
+Module_Particles::Module_Particles()
+    : _particle_system{create_particle_system()}
+{}
+
 Module_Particles::Module_Particles(Cool::DirtyFlagFactory_Ref dirty_flag_factory, Cool::InputFactory_Ref input_factory)
     : Module{"Nodes", dirty_flag_factory}
+    , _particle_system{create_particle_system()}
 {
+}
+
+auto Module_Particles::create_particle_system() const -> Cool::ParticleSystem
+{
+    return Cool::ParticleSystem{
+        _particles_count,
+        Cool::ParticlesShadersCode{
+            .simulation = *Cool::File::to_string(Cool::Path::root() / "res/Particles/simulation.comp"),
+            .vertex     = *Cool::File::to_string(Cool::Path::root() / "res/Particles/vertex.vert"),
+            .fragment   = *Cool::File::to_string(Cool::Path::root() / "res/Particles/fragment.frag"),
+        }
+    };
+}
+
+void Module_Particles::imgui_debug_menu()
+{
+    if (ImGui::DragScalar("Particles Count", ImGuiDataType_U64, &_particles_count))
+        recreate_particle_system();
+    if (ImGui::Button("Recreate Particle System"))
+        recreate_particle_system();
+}
+
+void Module_Particles::recreate_particle_system()
+{
+    _particle_system = create_particle_system();
 }
 
 void Module_Particles::update(UpdateContext_Ref)
--- src/Module_Particles/Module_Particles.h
+++ src/Module_Particles/Module_Particles.h
@@ -1,31 +1,35 @@
 #pragma once
+#include "Cool/Particles/ParticleSystem.h"
 #include "Module/Module.h"
-#include "ParticleSystem.h"
 
 namespace Lab {
 
 class Module_Particles : public Module {
 public:
-    Module_Particles() = default;
+    Module_Particles();
     explicit Module_Particles(Cool::DirtyFlagFactory_Ref, Cool::InputFactory_Ref);
     Module_Particles(Module_Particles const&)                        = default;
     auto operator=(Module_Particles const&) -> Module_Particles&     = default;
     Module_Particles(Module_Particles&&) noexcept                    = default;
     auto operator=(Module_Particles&&) noexcept -> Module_Particles& = default;
+    ~Module_Particles()                                              = default;
 
     void update(UpdateContext_Ref) override;
     void update_particles(UpdateContext_Ref);
     void imgui_windows(Ui_Ref, UpdateContext_Ref) const override;
     auto is_dirty(Cool::IsDirty_Ref) const -> bool override;
 
-    void render() { _particle_system.render(); }
-    void imgui_debug_menu() { _particle_system.imgui_debug_menu(); }
+    void imgui_debug_menu();
 
 private:
     void render(RenderParams, UpdateContext_Ref) override;
 
+    auto create_particle_system() const -> Cool::ParticleSystem;
+    void recreate_particle_system(); // TODO(Modules) Remove me, this is for tests only
+
 private:
-    ParticleSystem _particle_system{};
+    size_t               _particles_count{500};
+    Cool::ParticleSystem _particle_system;
 
 private:
     // Serialization
