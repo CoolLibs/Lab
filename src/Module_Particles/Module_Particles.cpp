0456706cd7db9788bf89dc5a912d4a7fb8679da5
Jules Fouchy <jules.fouchy@ntymail.com>
2023-12-04 15:05:02 +0100

    ✨ [Particles] Show error message when compilation fails

--- src/Module_Particles/Module_Particles.cpp
+++ src/Module_Particles/Module_Particles.cpp
@@ -1,5 +1,6 @@
 #include "Module_Particles.h"
 #include "Cool/File/File.h"
+#include "Cool/Log/ToUser.h"
 #include "Cool/Particles/ParticleSystem.h"
 
 namespace Lab {
@@ -14,16 +15,24 @@ Module_Particles::Module_Particles(Cool::DirtyFlagFactory_Ref dirty_flag_factory
 {
 }
 
-auto Module_Particles::create_particle_system() const -> Cool::ParticleSystem
+auto Module_Particles::create_particle_system() const -> std::optional<Cool::ParticleSystem>
 {
-    return Cool::ParticleSystem{
-        _particles_count,
-        Cool::ParticlesShadersCode{
-            .simulation = *Cool::File::to_string(Cool::Path::root() / "res/Particles/simulation.comp"),
-            .vertex     = *Cool::File::to_string(Cool::Path::root() / "res/Particles/vertex.vert"),
-            .fragment   = *Cool::File::to_string(Cool::Path::root() / "res/Particles/fragment.frag"),
-        }
-    };
+    try
+    {
+        return Cool::ParticleSystem{
+            _particles_count,
+            Cool::ParticlesShadersCode{
+                .simulation = *Cool::File::to_string(Cool::Path::root() / "res/Particles/simulation.comp"),
+                .vertex     = *Cool::File::to_string(Cool::Path::root() / "res/Particles/vertex.vert"),
+                .fragment   = *Cool::File::to_string(Cool::Path::root() / "res/Particles/fragment.frag"),
+            }
+        };
+    }
+    catch (std::exception const& e)
+    {
+        Cool::Log::ToUser::error("Particles Compilation Failed", e.what());
+        return {};
+    }
 }
 
 void Module_Particles::imgui_debug_menu()
@@ -45,7 +54,10 @@ void Module_Particles::update(UpdateContext_Ref)
 
 void Module_Particles::update_particles(UpdateContext_Ref)
 {
-    _particle_system.update();
+    if (!_particle_system)
+        return;
+
+    _particle_system->update();
 }
 
 void Module_Particles::imgui_windows(Ui_Ref ui, UpdateContext_Ref update_ctx) const
@@ -59,23 +71,26 @@ auto Module_Particles::is_dirty(Cool::IsDirty_Ref check_dirty) const -> bool
 
 void Module_Particles::render(RenderParams in, UpdateContext_Ref update_ctx)
 {
-    _particle_system.simulation_shader().bind();
-    _particle_system.simulation_shader().set_uniform("_time", in.provider(Cool::Input_Time{}));
-    _particle_system.simulation_shader().set_uniform("_delta_time", in.provider(Cool::Input_DeltaTime{}));
-    _particle_system.simulation_shader().set_uniform("_camera2D", in.provider(Cool::Input_Camera2D{}));
-    _particle_system.simulation_shader().set_uniform("_camera2D_inverse", glm::inverse(in.provider(Cool::Input_Camera2D{})));
-    _particle_system.simulation_shader().set_uniform("_height", in.provider(Cool::Input_Height{}));
-    _particle_system.simulation_shader().set_uniform("_aspect_ratio", in.provider(Cool::Input_AspectRatio{}));
+    if (!_particle_system)
+        return;
 
-    _particle_system.render_shader().bind();
-    _particle_system.render_shader().set_uniform("_time", in.provider(Cool::Input_Time{}));
-    _particle_system.render_shader().set_uniform("_delta_time", in.provider(Cool::Input_DeltaTime{}));
-    _particle_system.render_shader().set_uniform("_camera2D", in.provider(Cool::Input_Camera2D{}));
-    _particle_system.render_shader().set_uniform("_camera2D_inverse", glm::inverse(in.provider(Cool::Input_Camera2D{})));
-    _particle_system.render_shader().set_uniform("_height", in.provider(Cool::Input_Height{}));
-    _particle_system.render_shader().set_uniform("_aspect_ratio", in.provider(Cool::Input_AspectRatio{}));
+    _particle_system->simulation_shader().bind();
+    _particle_system->simulation_shader().set_uniform("_time", in.provider(Cool::Input_Time{}));
+    _particle_system->simulation_shader().set_uniform("_delta_time", in.provider(Cool::Input_DeltaTime{}));
+    _particle_system->simulation_shader().set_uniform("_camera2D", in.provider(Cool::Input_Camera2D{}));
+    _particle_system->simulation_shader().set_uniform("_camera2D_inverse", glm::inverse(in.provider(Cool::Input_Camera2D{})));
+    _particle_system->simulation_shader().set_uniform("_height", in.provider(Cool::Input_Height{}));
+    _particle_system->simulation_shader().set_uniform("_aspect_ratio", in.provider(Cool::Input_AspectRatio{}));
 
-    _particle_system.render();
+    _particle_system->render_shader().bind();
+    _particle_system->render_shader().set_uniform("_time", in.provider(Cool::Input_Time{}));
+    _particle_system->render_shader().set_uniform("_delta_time", in.provider(Cool::Input_DeltaTime{}));
+    _particle_system->render_shader().set_uniform("_camera2D", in.provider(Cool::Input_Camera2D{}));
+    _particle_system->render_shader().set_uniform("_camera2D_inverse", glm::inverse(in.provider(Cool::Input_Camera2D{})));
+    _particle_system->render_shader().set_uniform("_height", in.provider(Cool::Input_Height{}));
+    _particle_system->render_shader().set_uniform("_aspect_ratio", in.provider(Cool::Input_AspectRatio{}));
+
+    _particle_system->render();
 }
 
 } // namespace Lab
