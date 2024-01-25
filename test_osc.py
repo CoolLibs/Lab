32f420d22f9303d59576337dacf0690ddb3e4afa
Jules Fouchy <jules.fouchy@ntymail.com>
2024-01-25 09:24:46 +0100

    ✨ [OSC] Added OSC Node

--- test_osc.py
+++ test_osc.py
@@ -8,11 +8,11 @@ from pythonosc import udp_client
 if __name__ == "__main__":
     client = udp_client.SimpleUDPClient("127.0.0.1", 7000)
 
-    for x in range(100, 1000000000):
+    for x in range(0, 1000000000):
         val = x
         print(f"Sending {val}")
-        if x % 3 == 0:
-            client.send_message("dsfsdlkfhsdjvgejsjgvdjddsfdsfdsf", val / 100)
+        # if x % 3 == 0:
+        client.send_message("a", val / 10000)
         for name in ["/filter", "bob", "/lol"]:
             client.send_message(name, sin(val))
-            time.sleep(1)
+            time.sleep(0.1)
