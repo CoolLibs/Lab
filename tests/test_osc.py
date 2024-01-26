69a593e0269c7585a3c18806eb839d8bbff486d9
Jules Fouchy <jules.fouchy@ntymail.com>
2024-01-26 18:02:07 +0100

    🧼 [OSC] Cleanup

--- tests/test_osc.py
+++ tests/test_osc.py
@@ -1,18 +1,12 @@
-import random
 import time
 from math import sin
-
 from pythonosc import udp_client
-
 
 if __name__ == "__main__":
     client = udp_client.SimpleUDPClient("127.0.0.1", 7000)
-
     for x in range(0, 1000000000):
-        val = x
-        print(f"Sending {val}")
-        # if x % 3 == 0:
-        client.send_message("a", sin(val / 100) * 0.5 + 0.5)
+        print(f"Sending {x}")
+        client.send_message("a", sin(x / 100) * 0.5 + 0.5)
         for name in ["/filter", "bob", "/lol"]:
-            client.send_message(name, sin(val))
+            client.send_message(name, sin(x))
             time.sleep(0.1)
