34b2f641ff96fab6b36d5e491828d336fc6681bc
Jules Fouchy <jules.fouchy@ntymail.com>
2024-02-05 21:21:42 +0100

    🤏 [Command_Group] to_string() now displays the content of all commands + fix: execute and revert in right order

--- src/Commands/Command_Group.cpp
+++ src/Commands/Command_Group.cpp
@@ -1,4 +1,5 @@
 #include "Command_Group.h"
+#include <sstream>
 #include "CommandCore/LAB_REGISTER_COMMAND.h"
 #include "CommandCore/LAB_REGISTER_REVERSIBLE_COMMAND.h"
 
@@ -15,16 +16,20 @@ void Command_Group::execute(CommandExecutionContext_Ref const& ctx) const
 
 auto Command_Group::to_string() const -> std::string
 {
-    return fmt::format("Group");
+    std::stringstream ss{};
+    ss << "Group:";
+    for (auto const& command : commands)
+        ss << "\n  " << command->to_string();
+    return ss.str();
 }
 
 auto Command_Group::make_reversible(MakeReversibleCommandContext_Ref const& ctx) const
     -> ReversibleCommand_Group
 {
     auto res = ReversibleCommand_Group{};
-    for (auto it = commands.rbegin(); it != commands.rend(); ++it) // We want to undo in the reverse order compared to when we do
+    for (auto const& command : commands)
     {
-        auto reversible = (*it)->try_make_reversible(ctx);
+        auto reversible = command->try_make_reversible(ctx);
         if (!reversible)
             continue;
         res.commands.push_back(std::move(*reversible));
@@ -40,13 +45,17 @@ void ReversibleCommand_Group::execute(CommandExecutionContext_Ref const& ctx) co
 
 void ReversibleCommand_Group::revert(CommandExecutionContext_Ref const& ctx) const
 {
-    for (auto const& command : commands)
-        command->revert(ctx);
+    for (auto it = commands.rbegin(); it != commands.rend(); ++it) // We want to undo in the reverse order compared to when we do
+        (*it)->revert(ctx);
 }
 
 auto ReversibleCommand_Group::to_string() const -> std::string
 {
-    return "Group";
+    std::stringstream ss{};
+    ss << "Group:";
+    for (auto const& command : commands)
+        ss << "\n    " << command->to_string();
+    return ss.str();
 }
 
 auto ReversibleCommand_Group::merge(ReversibleCommand_Group const&) const -> std::optional<ReversibleCommand_Group>
