87ac0c848419c800a8807188123303cae728e662
Jules Fouchy <jules.fouchy@ntymail.com>
2024-03-05 16:38:13 +0100

    🔥 [UserSettings] Remove "Nodes developper mode"

--- src/Nodes/NodesLibraryManager.cpp
+++ src/Nodes/NodesLibraryManager.cpp
@@ -11,6 +11,10 @@ namespace Lab {
 void NodesLibraryManager::update(Cool::DirtyFlag const& regenerate_code_flag, Cool::NodesGraph& graph, NodesConfig const& nodes_config)
 {
     auto cfg = Cool::NodesConfig{nodes_config};
+#if !DEBUG
+    static bool is_first_frame{true};
+    if (is_first_frame) // In release, only check app's Nodes folder once, to load them all. And only user-data Nodes folder is hot-reloaded.
+#endif
     {
         auto updater = Cool::NodesDefinitionUpdater{cfg, graph, _nodes_library, &parse_node_definition, _nodes_folder_watcher.errors_map()};
         if (_nodes_folder_watcher.update(updater, [](std::filesystem::path const& path) { return NodesCategoryConfig{path}; }))
@@ -21,6 +25,9 @@ void NodesLibraryManager::update(Cool::DirtyFlag const& regenerate_code_flag, Co
         if (_user_nodes_folder_watcher.update(updater, [](std::filesystem::path const& path) { return NodesCategoryConfig{path}; }))
             regenerate_code_flag.set_dirty();
     }
+#if !DEBUG
+    is_first_frame = false;
+#endif
 }
 
 } // namespace Lab
