e9d04e6c8bd3184c838a08d915077ee897c60395
Jules Fouchy <jules.fouchy@ntymail.com>
2024-03-09 01:03:15 +0100

    ✨ [NodesParsing] Fix add_alpha_forwarding()

--- src/Nodes/CodeGen.cpp
+++ src/Nodes/CodeGen.cpp
@@ -17,51 +17,12 @@
 #include "FunctionSignature.h"
 #include "Node.h"
 #include "NodeDefinition.h"
+#include "gen_function_definition.h"
 #include "valid_glsl.h"
 #include "valid_input_name.h"
 #include "variable_to_primitive_type.h"
 
 namespace Lab {
-
-static auto gen_function_declaration(FunctionSignatureAsString const& signature, std::string const& unique_name) -> std::string
-{
-    using fmt::literals::operator""_a;
-    return fmt::format(
-        FMT_COMPILE(
-            R"STR({return_type} {name}/*needs_coollab_context*/({args}))STR"
-        ),
-        "return_type"_a = signature.return_type,
-        "name"_a        = unique_name, // We don't use signature.name because we need to use a unique name that has been generated for this instance of the function
-        "args"_a        = signature.arguments_list
-    );
-}
-
-struct Params_gen_function_definition {
-    FunctionSignatureAsString signature_as_string{};
-    std::string               unique_name{};
-    std::string               before_function{};
-    std::string               body{};
-};
-
-static auto gen_function_definition(Params_gen_function_definition const& p)
-    -> std::string
-{
-    using fmt::literals::operator""_a;
-    return {fmt::format(
-        FMT_COMPILE(
-            R"STR({before_function}
-            
-{declaration}
-{{
-    {body}
-}}
-)STR"
-        ),
-        "before_function"_a = p.before_function,
-        "declaration"_a     = gen_function_declaration(p.signature_as_string, p.unique_name),
-        "body"_a            = p.body
-    )};
-}
 
 static auto base_function_name(
     NodeDefinition const& definition,
