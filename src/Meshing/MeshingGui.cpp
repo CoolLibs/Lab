1327fbd56c08886bbaba35357409f30b58fd053e
Jules Fouchy <jules.fouchy@ntymail.com>
2024-07-12 19:18:56 +0200

    [Meshing] Example traversing the graph

--- src/Meshing/MeshingGui.cpp
+++ src/Meshing/MeshingGui.cpp
@@ -4,6 +4,7 @@
 #include "Cool/ImGui/IcoMoonCodepoints.h"
 #include "Cool/ImGui/icon_fmt.h"
 #include "Cool/Mesh/export_mesh.hpp"
+#include "Nodes/Node.h"
 #include "gen_mesh_from_sdf.hpp"
 
 namespace Lab {
@@ -21,6 +22,10 @@ void MeshingGui::open_window(Cool::NodeId const& main_node_id)
 
 static void gen_and_export_mesh(Cool::NodeId const& main_node_id, MeshingSettings const& meshing_settings, Cool::MeshExportSettings const& mesh_export_settings, DataToPassToShader const& data_to_pass_to_shader, DataToGenerateShaderCode const& data_to_generate_shader_code)
 {
+    // auto const maybe_node = data_to_generate_shader_code.nodes_graph.try_get_node<Node>(main_node_id);
+    // auto const node_def   = data_to_generate_shader_code.get_node_definition(maybe_node->id_names());
+    // is_shape_3D(node_def->signature());
+
     auto const maybe_mesh = gen_mesh_from_sdf(main_node_id, meshing_settings, data_to_pass_to_shader, data_to_generate_shader_code);
     if (!maybe_mesh)
         return; // TODO(Meshing) Error message should be handled here
--- src/Meshing/MeshingSettings.hpp
+++ src/Meshing/MeshingSettings.hpp
@@ -5,7 +5,8 @@ namespace Lab {
 struct MeshingSettings {
     glm::vec3  box_size{2.f};     // TODO(Meshing) Clarify : is this the radius or diameter of the box?
     glm::uvec3 samples_count{75}; /// Must always be >= 1 on each axis
-    auto       step_size() const -> glm::vec3 { return box_size / static_cast<glm::vec3>(samples_count - glm::uvec3{1}); }
+    // TODO(Meshing) expose step_size in the GUI, and deduce samples_count from it
+    auto step_size() const -> glm::vec3 { return box_size / static_cast<glm::vec3>(samples_count - glm::uvec3{1}); }
 
     void imgui();
 
