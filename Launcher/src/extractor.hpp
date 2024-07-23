870fb41ca15cfa7c34b773fcd945bb96032a200e
smallboyc <maxencedup44@gmail.com>
2024-07-23 17:31:26 +0200

    Move Launcher to a new repo

--- Launcher/src/extractor.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-#ifdef __APPLE__
-#include <sys/stat.h> // chmod
-#endif
-
-auto extract_zip(std::vector<char> const& zip, std::string_view const& version) -> void;
--- Launcher/src/release.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <nlohmann/json.hpp>
-#include <tl/expected.hpp>
-
-using json = nlohmann::json;
-
-auto get_release(std::string_view const& version) -> tl::expected<nlohmann::basic_json<>, std::string>;
-auto get_coollab_download_url(nlohmann::basic_json<> const& release) -> std::string;
