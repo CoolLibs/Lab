3e95788063b2e709f76ed19830a5689f6c6bcaf7
Jules Fouchy <jules.fouchy@ntymail.com>
2025-03-21 16:14:03 +0100

    💩 [Camera3DManager] Add inputs for position and rotation

--- src/Dependencies/Camera3DManager.cpp
+++ src/Dependencies/Camera3DManager.cpp
@@ -1,12 +1,14 @@
 #include "Camera3DManager.h"
-#include <Cool/Camera/ViewController_OrbitalU.h>
-#include <Cool/ImGui/IcoMoonCodepoints.h>
-#include <Cool/ImGui/icon_fmt.h>
 #include "CommandCore/CommandExecutor.h"
 #include "Commands/Command_FinishedEditingVariable.h"
 #include "Commands/Command_SetCameraZoom.h"
 #include "Commands/Command_SetVariable.h"
+#include "Cool/Camera/ViewController_OrbitalU.h"
+#include "Cool/ImGui/IcoMoonCodepoints.h"
+#include "Cool/ImGui/icon_fmt.h"
 #include "Cool/Utils/Chrono.h"
+#include "glm/gtx/euler_angles.hpp"
+#include "glm/gtx/matrix_decompose.hpp"
 
 namespace Lab {
 
@@ -74,6 +76,30 @@ void Camera3DManager::unhook_events(Cool::MouseEventDispatcher<Cool::ViewCoordin
 
 void Camera3DManager::imgui(CommandExecutor const& executor)
 {
+    maybe_update_camera(executor, [&](Cool::Camera& camera) {
+        bool b = false;
+
+        glm::vec3 scale;
+        glm::quat rotation;
+        glm::vec3 translation;
+        glm::vec3 skew;
+        glm::vec4 perspective;
+
+        glm::decompose(camera.view_matrix(), scale, rotation, translation, skew, perspective);
+
+        glm::vec3 eulerAnglesRadians = glm::eulerAngles(rotation);
+        b |= ImGui::DragFloat3("Position", glm::value_ptr(translation));
+        b |= ImGui::DragFloat3("Rotation", glm::value_ptr(eulerAnglesRadians));
+
+        if (b)
+        {
+            glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 rotationMatrix    = glm::eulerAngleXYZ(eulerAnglesRadians.x, eulerAnglesRadians.y, eulerAnglesRadians.z);
+            glm::mat4 scaleMatrix       = glm::scale(glm::mat4(1.0f), scale);
+            camera.set_view_matrix(translationMatrix * rotationMatrix * scaleMatrix);
+        }
+        return b;
+    });
     maybe_update_camera(executor, [&](Cool::Camera& camera) {
         return _view_controller.ImGui(camera);
     });
