24ce51c706a8c8ff7d15ef16506da6da4c3835ce
Jules Fouchy <jules.fouchy@ntymail.com>
2025-04-16 15:38:29 +0200

    Serialize test command

--- src/Dependencies/TestCommand.hpp
+++ src/Dependencies/TestCommand.hpp
@@ -4,5 +4,16 @@ namespace Lab {
 
 struct TestCommand {
     int bob{5};
+
+    // Serialization
+    friend class ser20::access;
+    template<class Archive>
+    void serialize(Archive& archive)
+    {
+        archive(
+            ser20::make_nvp("Bob", bob)
+        );
+    }
 };
+
 } // namespace Lab
